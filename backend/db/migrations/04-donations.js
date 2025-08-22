e7a5d7307f14b4f66d02d877f86a805b82872432
rmillay <rmmillay@gmail.com>
2025-08-21 17:50:05 -0700

    updated don fund table

--- backend/db/migrations/04-donations.js
+++ backend/db/migrations/04-donations.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "fundraisers",
+          model: "Fundraisers",
           key: "id"
         },
       }, 
