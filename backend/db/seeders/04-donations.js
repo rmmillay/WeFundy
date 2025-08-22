a86b474a1a0d3095c540fd40a192790c190dbb5b
rmillay <rmmillay@gmail.com>
2025-08-21 18:38:02 -0700

    updated don seeder table

--- backend/db/seeders/04-donations.js
+++ backend/db/seeders/04-donations.js
@@ -11,56 +11,56 @@ module.exports = {
     await Donation.bulkCreate(
       [
         {
-          
-          fundraiserId: 1, donorId: 1,
+          donorId: 1,
+          fundraiserId: 1, 
           donationAmount: 50.00,
           message: "Here ya go",
           //created_at: "2025-02-11",
           //updated_at: "2025-02-11"
         },
         { 
-          fundraiserId: 2,          donorId: 2,
-
+          donorId: 2,
+          fundraiserId: 2,
           donationAmount: 20.00,
           message: "Best wishes",
           //created_at: "2025-02-15",
           //updated_at: "2025-02-15"
         },
         {  
-          fundraiserId: 3,          donorId: 3,
-
+          donorId: 3,
+          fundraiserId: 3,
           donationAmount: 100.00,
           message: "Good luck",
           //created_at: "2025-02-18",
           //updated_at: "2025-02-18"
         },
         { 
-          fundraiserId: 4,          donorId: 4,
-
+          donorId: 4,
+          fundraiserId: 4,
           donationAmount: 35.00,
           message: "Hope this helps",
           //created_at: "2025-02-21",
           //updated_at: "2025-02-22"
         },
         {
-          fundraiserId: 5,          donorId: 5,
-
+          donorId: 5,
+          fundraiserId: 5,
           donationAmount: 15.00,
           message: "Great cause!",
           //created_at: "2025-02-25",
           //updated_at: "2025-02-28"
         }, 
         {
-          fundraiserId: 6,          donorId: 6,
-
+          donorId: 6,
+          fundraiserId: 6,
           donationAmount: 200.00,
           message: "Good ideas!",
           //created_at: "2025-03-01",
           //updated_at: "2025-03-01"
         },
         {
-          fundraiserId: 7,          donorId: 7,
-
+          donorId: 7,
+          fundraiserId: 7,
           donationAmount: 10.00,
           message: "I support that",
           //created_at: "2025-03-05",
