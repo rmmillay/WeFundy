908024318398c137c7b73802f13c40eb0debd135
rmillay <rmmillay@gmail.com>
2025-08-21 17:51:46 -0700

    updated comment fund table

--- backend/db/migrations/05-comments.js
+++ backend/db/migrations/05-comments.js
@@ -29,7 +29,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "fundraisers",
+          model: "Fundraisers",
           key: "id"
         },
       },
